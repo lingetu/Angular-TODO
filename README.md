2ba292067a1ac3cf257d8a490719d41661138e2b
LingETU <lucas.bondon@etu.univ-amu.fr>
2023-06-21 19:44:44 +0200

    todo

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# React first step
+# Angular first step
+
---
+++ todo/src/app/app.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { Item } from './item';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+
+  styles: ['h1 { color: red; }'],
+})
+
+export class AppComponent {
+  title = 'To do App';
+
+  filter: 'all' | 'active'| 'done' = 'all';
+
+  allItems = [
+    { description: 'eat', done: true },
+    { description: 'sleep', done: false },
+    { description: 'play', done: false },
+    { description: 'laugh', done: false },
+  ];
+
+
+
+  get items() {
+    if (this.filter == 'all') {
+      return this.allItems;
+    }
+    if(this.filter=="done")
+    {
+      return this.allItems.filter(item=>item.done==true);
+    }
+    return undefined;
+  }
+
+  addItem(description: string) {
+    this.allItems.unshift({ description, done: false });
+  }
+
+  remove(item: Item) {
+    this.allItems.splice(this.allItems.indexOf(item), 1);
+  }
+  
+ 
+}
+
